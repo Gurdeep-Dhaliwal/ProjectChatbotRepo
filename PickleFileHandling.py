ce3f496195ac0643684e24a5eaefef5c405bb696
Rafael de Moura Afonso Rodrigues (demourar) <demourar@coventry.ac.uk>
2019-10-28 22:33:26 +0000

    finally

---
+++ PickleFileHandling.py
@@ -0,0 +1,16 @@
+import pickle
+
+def FileRead(FileName):
+    FileRead=open(FileName,"rb")
+    Data=pickle.load(FileRead)
+    FileRead.close()
+    return Data
+
+def FileWrite(FileName,Data):
+    FileWrite=open(FileName,"wb")
+    pickle.dump(Data,FileWrite)
+    FileWrite.close()
+
+#KeyWordsList=FileRead("KeyWords")
+#print(KeyWordsList)
+#FileWrite("KeyWords",KeyWordsList)
